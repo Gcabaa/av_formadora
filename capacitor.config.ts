7ef4cb9eea34b197ca21baed5b6b9d079951db09
Gcabaa <maxbarreto324@gmail.com>
2024-03-31 19:33:16 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'av_formadora',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
